1f74fb6877291e85434386377f3b11b97865cb72
ragavragav430-coder <ragavragav430@gmail.com>
2026-07-22 10:11:22 +0530

    Update solution.sql

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,14 +1,3 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
-CREATE TABLE Student(
-    StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL,
-    DOB DATE,
-    Gender VARCHAR(10),
-    DepartmentID INT(5)
-);
 
 -- Alter Student table
 
@@ -17,3 +6,13 @@ CREATE TABLE Student(
 -- Add PhoneNumber
 
 -- Display structure
+
+USE  college;
+
+ALTER table student
+ADD email VARCHAR(30);
+
+ALTER TABLE student
+ADD phonenumber BIGINT;
+
+DESCRIBE student;
