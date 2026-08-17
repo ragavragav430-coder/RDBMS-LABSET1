3e8af9b85985367e92453a5b498fbd4d7ad683b3
ragavbca <ragavragav430@gmail.com>
2026-08-17 12:48:33 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -22,9 +22,8 @@ CREATE TABLE  student(
    StudentName VARCHAR(20)NOT NULL,
    DOB DATE NOT NULL,
    Gender VARCHAR(10)NOT NULL,
-   DepartmentID INT(50)NOT NULL
+   DepartmentID INT(5)NOT NULL
    );
-  
   
   
   
