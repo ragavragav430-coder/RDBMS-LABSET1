cce1a53baab65f721d2c4bf30d333959d2cb0cb8
ragavbca <ragavragav430@gmail.com>
2026-08-17 12:47:14 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -6,7 +6,9 @@
 
 -- Create Department Table
 
+use college;
+
 create database college_db;
 use college_db;
-CREATE TABLE department (department_id INT PRIMARY KEY, department_name VARCHAR(20), HOD VARCHAR(20));
+CREATE TABLE department (department_id INT PRIMARY KEY, department_name VARCHAR(100), HOD VARCHAR(100));
 SELECT* FROM DEPARTMENT;
