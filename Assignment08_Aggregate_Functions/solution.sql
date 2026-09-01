dfe8de39d30622f9a2b3f377bf1e4e3186544eae
ragavbca <ragavragav430@gmail.com>
2026-09-01 10:27:51 +0530

    Update solution.sql

--- Assignment08_Aggregate_Functions/solution.sql
+++ Assignment08_Aggregate_Functions/solution.sql
@@ -1,7 +1,3 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
 -- Create Employee table
 
 -- Insert records
@@ -13,3 +9,16 @@ USE CollegeDB;
 -- MIN()
 
 -- AVG()
+CREATE TABLE Employee (
+    EmployeeID INT,
+    EmployeeName VARCHAR(50),
+    Department VARCHAR(30),
+    Salary INT
+);
+
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary) VALUES
+(101, 'Ravi', 'HR', 25000),
+(102, 'Meena', 'IT', 40000),
+(103, 'Kumar', 'Finance', 35000),
+(104, 'Suresh', 'IT', 45000),
+(105, 'Latha', 'HR', 30000);
